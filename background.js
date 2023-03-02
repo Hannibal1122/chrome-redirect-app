7ae081109b63cf71141d57ecb89948aef87894a5
Mikhail <mwork92@gmail.com>
2023-03-02 17:33:32 +0300

    [update]

--- background.js
+++ background.js
@@ -1,100 +1,158 @@
-const map = {};
-let _id = 2;
+let _id;
+let rulesId = 2;
+let saveMap;
+let rules = {};
+const openSocketMap = new Map();
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ map });
+// Первоначальная инициализация
+chrome.storage.sync.get('map', async ({ map }) => {
+    saveMap = map;
+
+    if (!saveMap) {
+        saveMap = {};
+        chrome.storage.sync.set({ map: saveMap });
+    }
+    Object.keys(saveMap).forEach((key) => {
+        const { activate, id } = saveMap[key];
+        if (activate) connectTo(id);
+    });
+    const ids = Object.keys(saveMap).map((key) => key);
+    _id = ids.length ? Math.max(...ids) + 1 : 1;
+    console.log('ID', _id);
 });
 
-/* chrome.declarativeNetRequest.updateDynamicRules(); */
+chrome.runtime.onInstalled.addListener(() => {});
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.connectTo) {
-    connectTo(request.connectTo);
-    sendResponse({ state: "connect" });
-  }
-  if (request.appendRule) {
-    const { activate, angular, domain, fromSrc, name } = request.appendRule;
-    const id = _id++;
-    map[id] = { id, activate, angular, domain, fromSrc, name };
-    console.log("[Append]", activate, angular, domain, fromSrc, name);
-    chrome.storage.sync.set({ map });
+    if (request.appendRule) {
+        const { activate, angular, domain, fromDomain, fromBundle } = request.appendRule;
+        const id = _id++;
+        saveMap[id] = { id, activate, angular, domain, fromDomain, fromBundle };
+        console.log('[Append]', activate, angular, domain, fromDomain, fromBundle);
+        chrome.storage.sync.set({ map: saveMap });
 
-    if (activate) connectTo(domain);
-    sendResponse({ state: "append" });
-  }
-});
-
-chrome.storage.sync.get("map", async ({ map }) => {
-  console.log(map);
-  Object.keys(map).forEach((key) => {
-    const { activate, domain } = map[key];
-    if (activate) connectTo(domain);
-  });
-});
-
-function connectTo(src, id) {
-  let socket = new WebSocket(`ws://${src}/ws`);
-
-  console.log(socket);
-
-  socket.onopen = function (e) {
-    console.log("[open] Соединение установлено");
-    console.log("Отправляем данные на сервер");
-    socket.send("Меня зовут Джон");
-  };
-
-  socket.onmessage = function (event) {
-    /* console.log(`[message] Данные получены с сервера: ${event.data}`); */
-    const data = JSON.parse(event.data);
-    if (data.type == "hash") console.log(data.data);
-  };
-
-  socket.onclose = function (event) {
-    if (event.wasClean) {
-      console.log(
-        `[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`
-      );
-    } else {
-      // например, сервер убил процесс или сеть недоступна
-      // обычно в этом случае event.code 1006
-      console.log("[close] Соединение прервано");
+        if (activate) connectTo(id);
+        sendResponse({ state: 'append' });
     }
-  };
+    if (request.activateRule) {
+        const { id, activate } = request.activateRule;
 
-  socket.onerror = function (error) {
-    console.log(`[error]`);
-  };
+        saveMap[id].activate = activate;
+        chrome.storage.sync.set({ map: saveMap });
+
+        if (activate) connectTo(id);
+        else {
+            closeSocket(id);
+            removeRule(id, saveMap[id].domain);
+        }
+        sendResponse({ state: 'activate' });
+    }
+    if (request.removeRule) {
+        const { id } = request.removeRule;
+        delete saveMap[id];
+        chrome.storage.sync.set({ map: saveMap });
+        sendResponse({ state: 'remove' });
+    }
+    if (request.log) {
+        console.log(request.log);
+    }
+});
+
+function connectTo(id) {
+    const { domain } = saveMap[id];
+    if (openSocketMap.has(domain)) return;
+    const socket = new WebSocket(`ws://${domain}/ws`);
+    openSocketMap.set(domain, socket);
+
+    console.log(socket);
+
+    socket.onopen = function (e) {
+        console.log(`[${domain}] Соединение установлено`);
+    };
+
+    socket.onmessage = function (event) {
+        console.log(`[message] Данные получены с сервера: ${event.data}`);
+        const data = JSON.parse(event.data);
+        if (data.type == 'hash') {
+            appendRule(id, data.data, 'main');
+            appendRule(id, data.data, 'vendor');
+            appendRule(id, data.data, 'runtime');
+            // appendRule(id, data.data, 'polyfills');
+            console.log(`[${domain}] ${data.data}`);
+        }
+    };
+
+    socket.onclose = function (event) {
+        if (event.wasClean) {
+            console.log(`[${domain}] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+        } else {
+            // например, сервер убил процесс или сеть недоступна
+            // обычно в этом случае event.code 1006
+            console.log(`[${domain}] Соединение прервано`);
+        }
+        openSocketMap.delete(domain);
+    };
+
+    socket.onerror = function (error) {
+        console.log(`[${domain}] error`);
+    };
 }
 
-function createNewRedirectRule(
-  domain = "localhost",
-  fromSrc = "floor-3a8b8e9d4e09cfbc226e.js",
-  toSrc = "http://localhost:3001/assets/bundles/main-d266aca318608de9ec4a.js",
-  adblockRuleID = 2
-) {
-  return (
-    {
-      addRules: [
-        {
-          action: {
-            type: "redirect",
-            redirect: {
-              url: toSrc,
-            },
-          },
-          condition: {
-            urlFilter: fromSrc, // block URLs that starts with this
-            domains: [domain],
-            resourceTypes: ["script"],
-          },
-          id: adblockRuleID,
-          priority: 1,
-        },
-      ],
-      removeRuleIds: [adblockRuleID], // this removes old rule if any
-    },
-    () => {
-      console.log("block rule added");
+function closeSocket(id) {
+    const { domain } = saveMap[id];
+    if (!openSocketMap.has(domain)) return;
+    openSocketMap.get(domain).close();
+}
+
+function appendRule(id, hash, name) {
+    const { domain, fromDomain, fromBundle } = saveMap[id];
+    if (!rules[id]) rules[id] = {};
+
+    let ruleId = rules[id][name];
+    if (!ruleId) {
+        ruleId = rulesId++;
+        rules[id][name] = ruleId;
     }
-  );
+
+    let toSrc = `http://${domain}/assets/bundles/${name}-${hash}.js`;
+    const rule = createNewRedirectRule(fromDomain, fromBundle.replace('main-', name + '-'), toSrc, ruleId);
+    chrome.declarativeNetRequest.updateDynamicRules(rule, () => {
+        console.log(`[${domain}]`, fromDomain, `${name}-${hash}`, toSrc);
+    });
+}
+
+function removeRule(id, domain) {
+    if (rules[id]) {
+        chrome.declarativeNetRequest.updateDynamicRules(
+            {
+                removeRuleIds: Object.keys(rules[id]).map((key) => rules[id][key]),
+            },
+            () => {
+                console.log(`[${domain}]`, 'remove rule');
+            },
+        );
+    }
+}
+
+function createNewRedirectRule(fromDomain, fromBundle, toSrc, adblockRuleID) {
+    return {
+        addRules: [
+            {
+                action: {
+                    type: 'redirect',
+                    redirect: {
+                        url: toSrc,
+                    },
+                },
+                condition: {
+                    urlFilter: fromBundle, // block URLs that starts with this
+                    domains: [fromDomain],
+                    resourceTypes: ['script'],
+                },
+                id: adblockRuleID,
+                priority: 1,
+            },
+        ],
+        removeRuleIds: [adblockRuleID], // this removes old rule if any
+    };
 }
